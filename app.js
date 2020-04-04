c917222577307b64e6471628e125dcde618fa5e3
verils <verils@qq.com>
2020-04-05 00:53:12 +0800

    move config.js to root

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-const config = require('./app/config');
+const config = require('./config');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
